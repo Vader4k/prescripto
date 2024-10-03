a2faed76160f026cc8f64ff1302bc5fb796b4005
danie odukwe <kingdanny295@gmail.com>
2024-10-03 19:42:00 +0100

    @fix/ fixed duplicate error in schema and fixed security concerens with multer

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,11 +1,28 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ 
+  storage,
+  fileFilter: function (req, file, cb) {
+    const filetypes = /jpeg|jpg|png|gif/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    } else {
+      cb("Error: Images Only!");
+    }
+  },
+});
 
 export default upload;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -20,9 +20,6 @@ const userSchema = new mongoose.Schema(
       default:
         "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     },
-    address: {
-      type: Object,
-    },
     phone: {
       type: String,
       default: "0000000000",
