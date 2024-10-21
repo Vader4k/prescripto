15ff9e721c8293cc16135d6335eeb06c10d99a00
Vader4k <bigkingkush295@gmail.com>
2024-10-21 11:49:03 +0100

    @update/ changed text color for not available

--- frontend/src/components/DoctorCard.tsx
+++ frontend/src/components/DoctorCard.tsx
@@ -20,7 +20,7 @@ const DoctorCard = ({_id, name, image, speciality, availability}:DoctorCardProps
       <img className="bg-blue-50" src={image} alt={name} />
       <div className="p-4">
         <div className="flex items-center gap-2 text-sm text-center text-green-500">
-          <div className={`w-2 h-2 ${availability ? "bg-green-500" : "bg-red-500"} rounded-full`} />
+          <div className={`w-2 h-2 ${availability ? "bg-green-500" : "bg-gray-500"} rounded-full`} />
           <p>{availability ? "Available" : "Not Available"}</p>
         </div>
         <p className="font-medium text-gray-900 text-md">{name}</p>
